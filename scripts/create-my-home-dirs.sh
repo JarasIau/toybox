dd5a1e56bc2c0538c4f00cd6cce53a5c6d9909a9
JarasIau <jaraslau@tutanota.com>
2022-07-04 11:16:12 +0300

    Very minor changes

--- scripts/create-my-home-dirs.sh
+++ scripts/create-my-home-dirs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # Creates directories
-cd ~ && mkdir -p .walls .sources src etc prjcts music dwnld && \
-    cd etc && mkdir -p videos images audio && cd ~ || exit
+cd ~ && mkdir -p .walls .sources src etc projects music dwnld videos images audio
